63c7ab7b6945045d03950e74d830ad6f1150d959
jcshelley <azshelleyfamily@cox.net>
2019-01-04 19:50:36 -0700

    updated key path fp-key

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '3.82.228.152',
       username: 'ubuntu',
-      publicKey: 'https://travis-ci.org/jcshelley/starter-node-angular'
+      privateKey: 'https://travis-ci.org/jcshelley/starter-node-angular/fp-key'
     })
     .then(function() {
       console.log('SSH Connection established.');
